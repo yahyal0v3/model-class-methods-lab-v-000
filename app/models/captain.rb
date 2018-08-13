faf267c7b003f52bd8245c880276bde1a4d2e4e4
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-08-13 02:49:19 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,7 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    self.joins(:classifications).where(classifications: {name: "Catamaran"})
+    self.includes(:classifications).where(classifications: {name: "Catamaran"})
   end
 
   def self.sailors
